ed069ad94ed81f2e4c175a215804d9978420252a
hoan.lee <lonelywolf.lee@gmail.com>
2022-05-30 22:37:25 +0900

    Add haproxy

---
+++ example/haproxy/run-service.sh
@@ -0,0 +1,8 @@
+docker service create \
+--name gateway_proxy \
+--network ikare_api_gateway_network \
+-p 80:80/tcp \
+--mode global \
+--constraint 'node.platform.os == linux' \
+--mount type=bind,src=`pwd`/haproxy,dst=/usr/local/etc/haproxy/haproxy.cfg \
+haproxy:2.5.7
